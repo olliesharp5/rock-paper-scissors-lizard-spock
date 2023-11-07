5161cbe07ac7de2043f5a2b338876dbf4412aab0
olliesharp5 <>
2023-11-07 13:40:22 +0000

    Added aria labels and data choices to play buttons

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Declare constants for the DOM  
+*/
+const playerScore = document.getElementsByClassName("player-score-count");
+const botScore = document.getElementsByClassName("computer-score-count");
+const playerChoice = document.getElementById("player");
+const botChoice = document.getElementById("computer");
+const resetButton = document.getElementsByClassName("reset");
+
+const choices = [rock, paper, scissors, lizard, spock];
