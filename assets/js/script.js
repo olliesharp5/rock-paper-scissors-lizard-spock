c7b61438b4c233c402610d2cf68afe97f794f91b
olliesharp5 <>
2023-11-07 14:24:54 +0000

    Added style and js for modal popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,19 @@ const buttons = document.getElementsByClassName("btn");
 
 const choices = [rock, paper, scissors, lizard, spock];
 
+// Modal
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("rules");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+span.onclick = function () {
+    modal.style.display = "none";
+};
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
