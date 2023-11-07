309bc9dd93b4a73d0df3366004a23d8d6118bbc6
olliesharp5 <>
2023-11-07 16:03:22 +0000

    added event listener to buttons and function to display the users choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,12 @@
 */
 const playerScore = document.getElementsByClassName("player-score-count");
 const botScore = document.getElementsByClassName("computer-score-count");
-const playerChoice = document.getElementById("player");
-const botChoice = document.getElementById("computer");
+const playerChoice = document.getElementById("player-picture");
+const botChoice = document.getElementById("computer-picture");
 const resetButton = document.getElementsByClassName("reset");
 const buttons = document.getElementsByClassName("btn");
 
-const choices = [rock, paper, scissors, lizard, spock];
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 // Modal
 var modal = document.getElementById("myModal");
@@ -26,3 +26,25 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 };
+
+/**
+ * Event listener to identify which selection was made 
+ * based on which button was pressed 
+*/
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+        let playerChoice = this.getAttribute("data-choice");
+        runGame(playerChoice);
+    });
+}
+
+/**
+ * Function displays the user and computer selection 
+ * to the game area
+*/
+function runGame(playerChoice) {
+    let playerPicture = document.getElementById("player-picture");
+
+    playerPicture.src = `/assets/images/${choices[playerChoice]}.png`;
+    playerPicture.alt = choices[playerChoice];
+}
