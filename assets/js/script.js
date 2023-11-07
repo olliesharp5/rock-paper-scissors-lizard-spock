ef774d2aa099eecccc1220c2fd50acc3249d9423
olliesharp5 <>
2023-11-07 13:46:42 +0000

    Declared constants for the dom elements in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,6 @@ const botScore = document.getElementsByClassName("computer-score-count");
 const playerChoice = document.getElementById("player");
 const botChoice = document.getElementById("computer");
 const resetButton = document.getElementsByClassName("reset");
+const buttons = document.getElementsByClassName("btn");
 
 const choices = [rock, paper, scissors, lizard, spock];
